a3203340ae30a282a33d799fb9e265394b676abb
Kane Gall <kaneaus@gmail.com>
2025-05-23 16:16:24 -0400

    Initial Commit

---
+++ LiveLinkMusic/LiveLinkMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveLinkMusicApp.swift
+//  LiveLinkMusic
+//
+//  Created by Kane Gall on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveLinkMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
